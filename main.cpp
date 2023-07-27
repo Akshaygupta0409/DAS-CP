f370223ba35a5db708dce0307f2c9996b0d99618
akshay kumar gupta <guptaakshay0409@gmail.com>
2023-07-27 11:36:38 +0530

    grid dfs

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #define lb lower_bound
 #define ub upper_bound
 #define mk make_pair 
+#define pb push_back
 using namespace std;
 //basic setup --------------------------------------------------------------------------------------------------
 void setIO()
@@ -15,12 +16,20 @@ void setIO()
 #endif
 }
 //declaration of golbal variables and functions --------------------------------------------------------------------------------------------------
+const int INF = 1e18;
+const int NINF = -1e18;
+
+int n; // number of nodes
+int m; // number of edges
+vector<vector<int>> g;
+vector<int> color;
+vector<int> components;
+vector<string> s;
+using state = pair<int,int>;
+
+//helper functions ------------------------------------------------------------------------------------------------
 
 
-
-
-
-// helper definitions ---------------------------------------------------------------------------------------
 
 
 
@@ -28,9 +37,25 @@ void setIO()
 
 void solve()
 {
-    int n;
-    cin >>n; 
-    cout<<n;
+    cin>>n>>m;
+    g.resize(n ,vector<int>(m)); // 1 index;
+    // for(int i=0; i<m; i++){
+    //     int a , b; 
+    //     cin>>a>>b;
+    //     g[a].pb(b);
+    //     g[b].pb(a);
+    //     }
+
+     for(int i=0; i<n; i++){
+           s[i]="";
+         for(int i=0; i<m; i++){
+             char ch;
+             cin>>ch;
+             s[i] += ch;
+         }
+     }
+
+
     return;
 }
 
@@ -44,7 +69,7 @@ signed main()
     cout.tie(0);
     setIO();
     int t = 1;
-    // cin>>t;
+     //cin>>t;
     while (t--)
     {
         solve();
@@ -53,3 +78,9 @@ signed main()
     return 0;
 }
 // explations of code ------------------------------------------------------------------------------------------------
+/*
+
+
+
+
+*/
